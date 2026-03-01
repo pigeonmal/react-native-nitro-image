bac2b26a1677b7fdd337a7d15f0434e2d138740f
pigeonmal <eliastafnakji1@gmail.com>
2026-03-01 23:03:41 +0100

    chore: scope package

--- packages/react-native-nitro-image/src/OptionalWebLoader.ts
+++ packages/react-native-nitro-image/src/OptionalWebLoader.ts
@@ -1,31 +1,32 @@
 // biome-ignore lint/suspicious/noTsIgnore: Type Compilation is a race-condition
 // @ts-ignore
-type WebImagesType = typeof import('react-native-nitro-web-image')['WebImages']
+type WebImagesType =
+  (typeof import('@pigeonmal/react-native-nitro-web-image'))['WebImages'];
 type OptionalWebImagesType = Pick<
   WebImagesType,
   'createWebImageLoader' | 'loadFromURLAsync'
->
+>;
 
 let createWebImageLoader: WebImagesType['createWebImageLoader'] = () => {
   throw new Error(
     `Web Images are not supported because react-native-nitro-web-image is not installed!`,
-  )
-}
+  );
+};
 let loadFromURLAsync: WebImagesType['loadFromURLAsync'] = () => {
   throw new Error(
     `Web Images are not supported because react-native-nitro-web-image is not installed!`,
-  )
-}
+  );
+};
 
 export type OptionalAsyncOptions = Parameters<
   WebImagesType['loadFromURLAsync']
->[1]
+>[1];
 
 try {
-  const WebImages = require('react-native-nitro-web-image')
-    .WebImages as WebImagesType
-  createWebImageLoader = WebImages.createWebImageLoader.bind(WebImages)
-  loadFromURLAsync = WebImages.loadFromURLAsync.bind(WebImages)
+  const WebImages = require('@pigeonmal/react-native-nitro-web-image')
+    .WebImages as WebImagesType;
+  createWebImageLoader = WebImages.createWebImageLoader.bind(WebImages);
+  loadFromURLAsync = WebImages.loadFromURLAsync.bind(WebImages);
 } catch {
   // react-native-nitro-web-image is not installed, so only local images are supported.
 }
@@ -33,4 +34,4 @@ try {
 export const OptionalWebImages: OptionalWebImagesType = {
   createWebImageLoader,
   loadFromURLAsync,
-}
+};
